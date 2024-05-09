b098fbb93a81c106b51f3b29bcd1cd192a682e2f
kmkim0713 <kmkimworkwork@gmail.com>
2024-05-09 14:33:06 +0900

    Map을 이용한 반복분 예시

--- react-app/src/chapter_05/CommentList.jsx
+++ react-app/src/chapter_05/CommentList.jsx
@@ -1,11 +1,30 @@
 import React from "react"
 import Comment from "./Comment"
 
+const comments = [
+    {
+        name: "김경민1",
+        comment: "댓글 1"
+    },
+    {
+        name: "김경민2",
+        comment: "댓글 2"
+    },
+    {
+        name: "김경민3",
+        comment: "댓글 3"
+    }
+];
+
+
 function CommentList(props) {
     return (
         <div>
-            <Comment name="김경민" comment="나의 첫 댓글"/>
-            <Comment name="김경민2" comment="나의 첫 댓글2"/>
+            {comments.map((comment) => {
+                return (
+                    <Comment name={comment.name} comment={comment.comment} />
+                )
+            })}
         </div>
     );
 }
