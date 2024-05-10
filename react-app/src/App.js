7caedba0277ebba3b104cab07cc62ecc0dba837b
kmkim0713 <kmkimworkwork@gmail.com>
2024-05-10 09:28:38 +0900

    게시글 디자인

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -4,12 +4,22 @@ import './App.css';
 function App() {
 
   // tag에 클래스를 줄 때는 class가 아니라 className으로 줘야함
-  // js로 작성하는 것 이기 때문
+
+  // 변수 넣을 때는 중괄호(데이터 바인딩)
+  let post = '강남 우동 맛집';
+  let h4Id = 'aaa';
+
+  // style을 줄 때는 무조건 중괄호를 열고 객체로 전달해줘야한다
+  // style에서 "-" 기호는 사용할 수 없고 카멜케이스로 작성
 
   return (
     <div className="App"> 
       <div className="black-nav">
-        <h4>블로그</h4>
+        <h4 id={h4Id} style={{color:'red'}}>블로그</h4>
+      </div>
+      <div className='list'>
+        <h4>글제목</h4>
+        <p>2월 17일 발행</p>
       </div>
     </div>
   );
